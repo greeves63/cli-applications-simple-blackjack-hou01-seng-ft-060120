56cbbf2e8f02c1f1515be32b17966e9aa9f65515
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-28 03:43:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,7 +46,9 @@ def hit?(card_total)
 end
 
 def invalid_command
-  
+  puts "Please enter a valid command"
+  prompt_user
+  get_user_input
 end
 
 #####################################################
