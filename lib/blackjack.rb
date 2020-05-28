38d1d05fd30faf137b516a424398d22fed22836e
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-28 03:51:04 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,10 +49,7 @@ def invalid_command
   puts "Please enter a valid command"
   prompt_user
   get_user_input
-  if user_input == "h"
-    card_total += deal_card 
-  elsif user_input == "s"
-    card_total 
+  
 end
 
 #####################################################
